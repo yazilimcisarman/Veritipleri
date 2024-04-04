e378b3aa7ba131e389352d90ab32eca946ea5f61
Muhammet Seyda Armağan <muhammedseyda@hotmail.com>
2024-04-04 11:38:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veritipleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int char string bool double float
+
+
+            int a = 3;
+            char c = 'a';
+            string d = "AboneOl";
+            bool w = true;
+            float b = 3.14f;
+            double r = 3.14;
+            var deneme = 3.14f;
+
+            Console.WriteLine(a);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(w);
+            Console.WriteLine(b);
+            Console.WriteLine(r);
+            Console.ReadLine();
+        }
+    }
+}
